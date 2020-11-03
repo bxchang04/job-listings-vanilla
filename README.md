77eb4d2b5700bc541be7022a445c06b1c42688a7
bxchang04 <bxchang04@gmail.com>
2020-11-03 07:57:09 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Job Listings Filter Page challange by FrontendMentor
+
+![Design preview for the Job Listings coding challenge](./design/desktop-preview.jpg)
---
+++ index.js
@@ -0,0 +1,253 @@
+//to-dos
+/*
+0. spend an hour finding tutorials and alternative solutions to this vanilla JS for filtering a list
+
+1. change styling for filter buttons
+    -make this easier to hover over as well
+
+2. change clear button styling
+    -change inactive color as well
+
+3. add Sass
+
+*/
+
+// Filters
+const filtersSelected = new Set();
+
+// queryselectors
+const filters = document.querySelector('.filter-cover');
+const clear = document.querySelector('.clear');
+
+const bgImg = document.getElementById('bg-img');
+
+// media query?
+if(window.innerWidth <= 540 ) {
+    let bg = bgImg.src.split('/') ;
+    console.log(bg , bg.length );
+    bg[bg.length-1] = 'bg-header-mobile.svg' ;
+    bgImg.src = bg.join('/');
+}
+
+
+const filterParent = filters.parentElement;
+
+// destructuring?
+let data, main = document.querySelector('main'), realJobList = [];
+
+// API call using fetch
+fetch('data.json')
+    .then(data => data.json())
+    .then(res => {
+        data = res;
+        createList(data);
+    });
+
+// Create list of filtered tags?
+
+function createList(data) {
+
+    // filterParent.classList.add('none'); // leftover. Ugly.
+
+    let docf = document.createDocumentFragment();
+    data.forEach(el => {
+        let listing = document.createElement('div');
+        listing.classList.add('listing');
+
+        let listFrag = document.createDocumentFragment();
+        listing.companyName = el.company
+        listing.data_filters = new Set([...el.languages, ...el.tools, el.role, el.level]);
+        listing.data_filters.forEach(l => {
+            let li = document.createElement('li');
+            li.classList.add('job__filter');
+            li.textContent = l;
+            li.setAttribute('data-filter', l);
+            listFrag.appendChild(li);
+        })
+
+        listing.innerHTML = `
+        <div class="left">
+            <div class="listing-img">
+                <img src="${el.logo}" alt="">
+            </div>
+            <div class="company-details">
+                <div class="company-nametags">
+                    <h4 class="company-name">${el.company}</h4>
+                    <div class="company-tags">
+                        <p class="company-tag-new" style="display:${ el.new ? "block" : 'none'}" >NEW</p>
+                        <p class="company-tag-ftrd" style="display:${ el.featured ? "block" : 'none'}" >FEATURED</p>
+                    </div>
+                </div>
+                <h2 class="job__role">${el.position}</h2>
+                <div class="job__min-details">
+                    <p class="job__time">${el.postedAt}</p>
+                    <p class="contOrFull">${el.contract}</p>
+                    <p class="job__location">${el.location}</p>
+                </div>
+            </div>
+        </div>
+        <ul class="job__filters">
+        </ul>
+        ` ;
+
+        // what do these three lines do???
+        listing.querySelector('.job__filters').append(listFrag);
+        realJobList.push(listing);
+        docf.append(listing);
+    });
+
+    // What is this?
+    main.append(docf);
+
+    // Nani?
+    const filterBtns = document.querySelectorAll('.job__filters > .job__filter');
+
+    filterBtns.forEach(el => {
+        el.addEventListener('click', ev => {
+            addFilter(el.dataset.filter);
+        })
+    })
+}
+
+// Add event listeners for filter buttons
+
+function addFilter(filter) {
+
+    if (filtersSelected.has(filter)) {
+        return;
+    }
+
+    if (filtersSelected.size == 0) {
+        filterParent.classList.remove('none');
+        main.classList.add('main-space') ;
+    }
+
+    let newFilter = document.createElement('div');
+
+    newFilter.classList.add('filter');
+
+    newFilter.textContent = filter;
+
+    newFilter.addEventListener('click', filterClick);
+
+    newFilter.setAttribute('data-filter', filter);
+
+    filters.append(newFilter);
+
+    filtersSelected.add(filter);
+
+    filterJobs();
+
+}
+
+// Actually filter each job based on filtersSelected
+    // When commented out:
+        // clear button breaks
+        // jobs aren't filtered on click
+
+function filterJobs() {
+
+    realJobList.forEach(job => {
+
+        // uses helper functions
+        if (check(filtersSelected, job.data_filters)) { 
+            showJob(job);
+        } else {
+            hideJob(job);
+        }
+    })
+}
+
+// Added to filters div (onClick event for event listeners above)
+    // Test: When commented out, tags aren't added to list on click. Also, clear button fails to close list (and remove buttons?).
+
+function filterClick(ev) {
+
+    filtersSelected.delete(this.dataset.filter);
+
+    console.log(filtersSelected) ; // not sure why this doesn't output to console
+
+    if (filtersSelected.size == 0) {
+        filterParent.classList.add('none');
+        main.classList.remove('main-space') ;
+    }
+    this.remove();
+    filterJobs(this.dataset.filter);
+}
+
+// Transitions. !!! Be careful about copying verbatim here!!!
+
+function setTransitionEvent(){
+    var t;
+    var el = document.createElement('fakeelement');
+    var transitions = {
+      'WebkitTransition' :'webkitTransitionEnd',
+      'MozTransition'    :'transitionend',
+      'MSTransition'     :'msTransitionEnd',
+      'OTransition'      :'oTransitionEnd',
+      'transition'       :'transitionEnd'
+    }
+  
+    for(t in transitions){
+      if( el.style[t] !== undefined ){
+        return transitions[t];
+      }
+    }
+}
+
+// Begin: Helper functions
+function hideJob(job) {
+    let event = setTransitionEvent() ;
+    job.classList.add('fade');
+    job.addEventListener(`${event}`, ev => job.classList.add('none'), { once: true });
+}
+
+
+function showJob(job) {
+    job.classList.remove('none');
+    job.classList.add('fadeAnim');
+    job.addEventListener('animationend', ev => job.classList.remove('fade', 'fadeAnim'), { once: true })
+}
+
+
+function check(set1, set2) {
+    // to check if set2 contains all elements of set1
+    let bool = false;
+    if (set1.size == 0) {
+        return true;
+    }
+    let i = 0;
+    set1.forEach(s => {
+        if (i == 0) {
+            if (set2.has(s)) {
+                bool = true;
+            } else {
+                i++;
+                bool = false;
+            }
+        }
+    })
+    return bool;
+}
+// End: Helper functions
+
+// Begin: Clear button logic
+    //for cleanup?
+clear.addEventListener('click', ev => {
+    if (filtersSelected.size == 0) {
+        return;
+    }
+    filtersSelected.clear();
+    filterJobs();
+    clearFilters();
+})
+
+function clearFilters() {
+    while (filters.firstElementChild) {
+        filters.lastElementChild.remove();
+    }
+    filterParent.classList.add('none');
+    main.classList.remove('main-space') ;
+}
+
+// End: Clear button logic
